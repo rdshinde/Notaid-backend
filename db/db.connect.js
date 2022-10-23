b24ad96bb7700439d3a271a3e1f1a85be11c95ba
rishikesh.s <rdshinde2000@gmail.com>
2022-10-23 23:04:17 +0530

    Inital Commit

---
+++ db/db.connect.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbPWD = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+
+const mongoDBURI = process.env.MONGO_URI;
+
+const mongoURI =
+  `mongodb+srv://${dbUser}:${dbPWD}@cluster0.arkjirn.mongodb.net/test` ||
+  mongoDBURI;
+
+const connectDB = async () => {
+  await mongoose
+    .connect(mongoURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log("connected to db!"))
+    .catch((e) => console.dir(e));
+};
+module.exports = { connectDB };
+
+// mongodb+srv://${dbUser}:${dbPWD}@cluster0.arkjirn.mongodb.net/test
---
+++ utils/index.js
@@ -0,0 +1,16 @@
+const DocsObj = {
+  title: "NotAid",
+  description: "The modern note taking app.",
+  author: "Rishikesh Shinde",
+  features: [],
+  apiEndpoints: [
+    {
+      api: "/",
+      method: "GET",
+      description:
+        "This endpoint is used for details about backend functionality.",
+    },
+  ],
+};
+
+module.exports = { DocsObj };
